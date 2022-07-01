caa0a5ba95e5bbfea1d4f8b3005238d7536abef7
narbat <108547227+narbat@users.noreply.github.com>
2022-07-01 22:09:30 +0300

    Update boboasteikko.js

--- boboasteikko.js
+++ boboasteikko.js
@@ -5,7 +5,6 @@ const positiv = document.getElementById("positiv")
 positiv.addEventListener("click", () => {
     bobo = bobo + 1;
     localStorage.answer = JSON.stringify(bobo);
-    alert(bobo);
 })
 
 
@@ -13,5 +12,4 @@ const neg = document.getElementById("neg")
 neg.addEventListener("click", () => {
     bobo = bobo - 1;
     localStorage.answer = JSON.stringify(bobo);
-    alert(bobo);
 })
