2b87dd89b436d789a843fb1cb007d5c29813907a
Narbat <niklasnummela62@gmail.com>
2022-07-01 21:37:26 +0300

    goofy

---
+++ boboasteikko.js
@@ -0,0 +1,22 @@
+var bobo = JSON.parse(localStorage.answer);
+
+
+const positiv = document.getElementById("positiv")
+positiv.addEventListener("click", () => {
+    bobo = bobo + 1;
+    localStorage.answer = JSON.stringify(bobo);
+    alert(bobo);
+})
+
+
+const neg = document.getElementById("neg")
+neg.addEventListener("click", () => {
+    bobo = bobo - 1;
+    localStorage.answer = JSON.stringify(bobo);
+    alert(bobo);
+})
+
+const neutr = document.getElementById("neutr")
+neutr.addEventListener("click", () => {
+    alert(bobo);
+})
